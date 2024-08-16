f09d4cdac661995e5bfc24f66626a9f07803e31f
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2024-08-16 08:55:01 +0800

    Implement serializes for Survey responses
    Along with validation
    
    Signed-off-by: Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>

--- surveys/serializers.py
+++ surveys/serializers.py
@@ -31,7 +31,7 @@ class FieldSerializer(serializers.ModelSerializer):
                     "options": "This field is only required for dropdown, radio, or checkbox fields."
                 }
             )
-        return data
+        return super().validate(data)
 
     def create(self, validated_data):
         fields_data = validated_data.pop("fields", [])
@@ -88,7 +88,7 @@ class SurveySerializer(serializers.ModelSerializer):
         survey = Survey.objects.create(**validated_data)
         for section_data in sections_data:
             section_serializer = SectionSerializer(data=section_data)
-            if section_serializer.is_valid():
+            if section_serializer.is_valid(raise_exception=True):
                 section_serializer.save(survey=survey)
         return survey
 
@@ -107,11 +107,11 @@ class SurveySerializer(serializers.ModelSerializer):
                     SectionSerializer().update(section_instance, section_data)
                 else:
                     section_serializer = SectionSerializer(data=section_data)
-                    if section_serializer.is_valid():
+                    if section_serializer.is_valid(raise_exception=True):
                         section_serializer.save(survey=instance)
             else:
                 section_serializer = SectionSerializer(data=section_data)
-                if section_serializer.is_valid():
+                if section_serializer.is_valid(raise_exception=True):
                     section_serializer.save(survey=instance)
 
         return instance
@@ -122,6 +122,72 @@ class FieldResponseSerializer(serializers.ModelSerializer):
         model = FieldResponse
         exclude = ["section_response"]
 
+    def to_internal_value(self, data):
+        if isinstance(data.get("field"), Field):
+            data["field"] = data["field"].id
+        return super().to_internal_value(data)
+
+    def validate(self, attrs):
+        field = attrs.get("field")
+        value = attrs.get("value")
+
+        # Check if the field is required and if the value is missing
+        if field.required and not value:
+            raise serializers.ValidationError({"value": "This field is required."})
+
+        field_type = field.field_type
+        options = field.options or []
+
+        if field_type == "text":
+            if not isinstance(value, str):
+                raise serializers.ValidationError(
+                    "Expected a string for text field type."
+                )
+        elif field_type == "number":
+            try:
+                float(value)
+            except (TypeError, ValueError):
+                raise serializers.ValidationError(
+                    "Expected a number for number field type."
+                )
+
+        elif field_type == "date":
+            try:
+                # Assuming the date is provided as a string in 'YYYY-MM-DD' format
+                from datetime import datetime
+
+                datetime.strptime(value, "%Y-%m-%d")
+            except (TypeError, ValueError):
+                raise serializers.ValidationError(
+                    "Expected a date in 'YYYY-MM-DD' format for date field type."
+                )
+
+        elif field_type in ["dropdown", "radio"]:
+            if not isinstance(value, str):
+                raise serializers.ValidationError(
+                    f"Expected a single string value for {field_type} field type."
+                )
+            if value not in options:
+                raise serializers.ValidationError(
+                    f"Value '{value}' is not a valid option for {field_type} field type."
+                )
+
+        elif field_type == "checkbox":
+            if not isinstance(value, list):
+                raise serializers.ValidationError(
+                    "Expected a list of values for checkbox field type."
+                )
+            invalid_options = [v for v in value if v not in options]
+            if invalid_options:
+                raise serializers.ValidationError(
+                    f"Invalid options {invalid_options} for checkbox field type."
+                )
+
+        else:
+            raise serializers.ValidationError("Unsupported field type.")
+
+        return super().validate(attrs)
+
 
 class SectionResponseSerializer(serializers.ModelSerializer):
     field_responses = FieldResponseSerializer(many=True)
@@ -130,6 +196,68 @@ class SectionResponseSerializer(serializers.ModelSerializer):
         model = SectionResponse
         exclude = ["survey_response"]
 
+    def to_internal_value(self, data):
+        if isinstance(data.get("section"), Section):
+            data["section"] = data["section"].id
+        return super().to_internal_value(data)
+
+    def validate(self, data):
+        section = data["section"]
+        field_responses_data = data.get("field_responses", [])
+        field_responses_ids = {
+            response["field"].id for response in field_responses_data
+        }
+        all_fields = set(section.fields.values_list("id", flat=True))
+        required_fields = set(
+            section.fields.filter(required=True).values_list("id", flat=True)
+        )
+
+        invalid_fields = field_responses_ids - all_fields
+
+        if invalid_fields:
+            raise serializers.ValidationError(
+                f"The following fields are not part of the section: {', '.join(map(str, invalid_fields))}"
+            )
+
+        if data.get("completed", False):
+            # Ensure all required fields are filled out
+            missing_required_fields = required_fields - field_responses_ids
+
+            if missing_required_fields:
+                raise serializers.ValidationError(
+                    f"The following required fields are missing in the response: {', '.join(map(str, missing_required_fields))}"
+                )
+
+        return super().validate(data)
+
+    def create(self, validated_data):
+        field_responses_data = validated_data.pop("field_responses", [])
+        section_response = SectionResponse.objects.create(**validated_data)
+        for field_response_data in field_responses_data:
+            FieldResponse.objects.create(
+                section_response=section_response, **field_response_data
+            )
+
+        return section_response
+
+    def update(self, instance, validated_data):
+        field_responses_data = validated_data.pop("field_responses", [])
+        instance.completed = validated_data.get("completed", instance.completed)
+
+        # Handle nested field responses
+        for field_response_data in field_responses_data:
+            field_response_instance = instance.field_responses.filter(
+                id=field_response_data.get("id")
+            ).first()
+            if field_response_instance:
+                FieldResponseSerializer().update(
+                    field_response_instance, field_response_data
+                )
+            else:
+                FieldResponse.objects.create(
+                    section_response=instance, **field_response_data
+                )
+
 
 class SurveyResponseSerializer(serializers.ModelSerializer):
     section_responses = SectionResponseSerializer(many=True)
@@ -137,3 +265,72 @@ class SurveyResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = SurveyResponse
         fields = "__all__"
+
+    def validate(self, data):
+        survey = data["survey"]
+        section_responses_data = data.get("section_responses", [])
+        section_responses_ids = {
+            response["section"].id for response in section_responses_data
+        }
+
+        # Get all section IDs for the given survey
+        valid_section_ids = set(survey.sections.values_list("id", flat=True))
+
+        # Check for any section in the responses that is not part of the survey
+        invalid_sections = section_responses_ids - valid_section_ids
+
+        if invalid_sections:
+            raise serializers.ValidationError(
+                f"Some sections in the response do not belong to the survey: {', '.join(map(str, invalid_sections))}"
+            )
+
+        if data.get("completed", False):
+            section_responses_dict = {
+                response["section"].id: response for response in section_responses_data
+            }
+
+            for section_id in valid_section_ids:
+                if section_id not in section_responses_dict:
+                    raise serializers.ValidationError(
+                        f"Section with ID {section_id} is missing in the response."
+                    )
+                # Validate that the section response is completed
+                if not section_responses_dict[section_id].get("completed", False):
+                    raise serializers.ValidationError(
+                        f"Section with ID {section_id} must be completed before marking the survey as completed."
+                    )
+
+        return super().validate(data)
+
+    def create(self, validated_data):
+        section_responses_data = validated_data.pop("section_responses", [])
+        survey_response = SurveyResponse.objects.create(**validated_data)
+
+        for section_response_data in section_responses_data:
+            section_response_serializer = SectionResponseSerializer(
+                data=section_response_data
+            )
+            if section_response_serializer.is_valid(raise_exception=True):
+                section_response_serializer.save(survey_response=survey_response)
+
+        return survey_response
+
+    def update(self, instance, validated_data):
+        section_responses_data = validated_data.pop("section_responses", [])
+
+        instance.completed = validated_data.get("completed", instance.completed)
+
+        for section_response_data in section_responses_data:
+            section_response_instance = instance.section_responses.filter(
+                id=section_response_data.get("id")
+            ).first()
+            if section_response_instance:
+                SectionResponseSerializer().update(
+                    section_response_instance, section_response_data
+                )
+            else:
+                section_response_serializer = SectionResponseSerializer(
+                    data=section_response_data
+                )
+                if section_response_serializer.is_valid(raise_exception=True):
+                    section_response_serializer.save(survey_response=instance)
